816002e54d0178442e7e8def65a2e7685837ac7c
olumayokunolayinka <olumayokunolayinka@gmail.com>
2020-08-29 12:21:23 +0100

    refix

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
+<<<<<<< Updated upstream
 
+=======
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+>>>>>>> Stashed changes
 
 ReactDOM.render(
   <React.StrictMode>
